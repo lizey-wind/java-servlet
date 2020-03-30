ef61e4a85109dd591e7bcf085beb9184b5b6b85a
Wind <1625475704@qq.com>
2020-03-30 12:30:33 +0800

    first commit

---
+++ src/com/runoob/test/HelloWorld.java
@@ -0,0 +1,27 @@
+package com.runoob.test;
+
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HelloWorld  extends HttpServlet {
+    private String message;
+    public void init() throws ServletException {
+        message = "Hello World";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        response.setContentType("test/html");
+        PrintWriter out = response.getWriter();
+        out.println("<h1>" + message + "</h1>");
+    }
+
+    public void destroy() {
+
+    }
+}
